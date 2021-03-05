af2a579334ccdb6cdbfc3d12df7f38422dfca092
ifucolo <ifucolo>
2021-03-05 19:52:07 +0100

    count down timer

---
+++ app/src/main/java/com/example/androiddevchallenge/screen/CountDown.kt
@@ -0,0 +1,278 @@
+package com.example.androiddevchallenge.screen
+
+import android.os.CountDownTimer
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.ui.theme.shapes
+
+
+@ExperimentalAnimationApi
+@Composable
+fun CountDownScreen() {
+    val countMinutes = remember { mutableStateOf(0) }
+    val countSeconds = remember { mutableStateOf(0) }
+    val showDuringProgress = remember { mutableStateOf(false)}
+    val maxMilliSeconds = remember { mutableStateOf(0L) }
+    val steps = remember { mutableStateOf(0L) }
+
+    val progress = remember { mutableStateOf(0.0001f) }
+
+    val animatedProgress = animateFloatAsState(
+        targetValue = progress.value,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    ).value
+
+    val countDownTimer = object : CountDownTimer(maxMilliSeconds.value, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            val increment = 1.0 / steps.value
+            progress.value = (increment + progress.value).toFloat()
+            println("progress ${progress.value}")
+        }
+
+        override fun onFinish() {
+            progress.value = 1f
+            resetTimer(
+                countMinutes = countMinutes,
+                countSeconds = countSeconds,
+                progress = progress,
+                showDuringProgress = showDuringProgress
+            )
+
+            cancel()
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.padding(30.dp))
+
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp),
+            progress = animatedProgress
+        )
+        AnimatedVisibility(visible = !showDuringProgress.value) {
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                TimeSelectorComponent(
+                    timeUnit = TimeUnit.Minutes,
+                    count = countMinutes.value,
+                    onCountChange = {
+                        countMinutes.value = it
+                        maxMilliSeconds.value = getProgressInSeconds(mins = countMinutes.value, secs = countSeconds.value).toLong()
+                        steps .value= maxMilliSeconds.value/1000
+                    },
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                TimeSelectorComponent(
+                    timeUnit = TimeUnit.Seconds,
+                    count = countSeconds.value,
+                    onCountChange = {
+                        countSeconds.value = it
+                        maxMilliSeconds.value = getProgressInSeconds(mins = countMinutes.value, secs = countSeconds.value).toLong()
+                        steps.value = maxMilliSeconds.value/1000
+                    },
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        AnimatedVisibility(visible = !showDuringProgress.value) {
+            Button(
+                enabled = countMinutes.value > 0 || countSeconds.value > 0,
+                onClick = {
+                    showDuringProgress.value = true
+                    countDownTimer.start()
+                },
+            ) {
+                Text(text = "START")
+            }
+        }
+    }
+}
+
+fun resetTimer(
+    countMinutes: MutableState<Int>,
+    countSeconds: MutableState<Int>,
+    progress: MutableState<Float>,
+    showDuringProgress: MutableState<Boolean>
+) {
+    countMinutes.value = 0
+    countSeconds.value = 0
+
+    progress.value = 0.0001f
+    showDuringProgress.value =  false
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun TimeSelectorComponent(
+    timeUnit: TimeUnit,
+    count: Int,
+    onCountChange: (Int) -> Unit
+) {
+    val canClickPlus = remember { mutableStateOf(true)}
+    val canClickMinus = remember { mutableStateOf(false)}
+
+    val maxPlus = when(timeUnit) {
+        TimeUnit.Minutes -> MAX_MINUTES
+        TimeUnit.Seconds -> MAX_SECONDS
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.height(300.dp)
+    ) {
+        val type = when(timeUnit) {
+            TimeUnit.Minutes -> "Min"
+            TimeUnit.Seconds -> "Sec"
+        }
+
+        Text(
+            text = type,
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                color = Color.White,
+                fontWeight = FontWeight.Light,
+                fontSize = 12.sp
+            )
+        )
+        Text(
+            text = makeTimeText(count),
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        )
+        Spacer(modifier = Modifier.padding(4.dp))
+        AnimatedVisibility(visible = canClickPlus.value) {
+            FloatingActionButton(
+                onClick = {
+                    handleTime(
+                        operations = Operations.Plus,
+                        count = count,
+                        onCountChange = {
+                          onCountChange.invoke(it)
+                        },
+                        canClickMinus = canClickMinus,
+                        canClickPlus = canClickPlus,
+                        maxPlus = maxPlus
+                    )
+                },
+                modifier = Modifier
+                    .height(30.dp)
+                    .width(30.dp)
+                    .clip(shape = shapes.medium),
+                backgroundColor = MaterialTheme.colors.secondary
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_exposure_plus_1_24),
+                    contentDescription = "plus"
+                )
+            }
+        }
+        Spacer(modifier = Modifier.padding(8.dp))
+        AnimatedVisibility(visible = canClickMinus.value) {
+            FloatingActionButton(
+                onClick = {
+                    handleTime(
+                        operations = Operations.Minus,
+                        count = count,
+                        onCountChange = {
+                            onCountChange.invoke(it)
+                        },
+                        canClickMinus = canClickMinus,
+                        canClickPlus = canClickPlus,
+                        maxPlus = maxPlus
+                    )
+
+                },
+                modifier = Modifier
+                    .height(30.dp)
+                    .width(30.dp)
+                    .clip(shape = shapes.medium),
+                backgroundColor = MaterialTheme.colors.secondary
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_exposure_neg_1_24),
+                    contentDescription = "minus"
+                )
+            }
+        }
+    }
+}
+
+
+fun getProgressInSeconds(mins: Int, secs: Int): Int =
+    ((mins * 60) + secs) * 1000
+
+fun handleTime(
+    operations: Operations,
+    count: Int,
+    onCountChange: (Int) -> Unit,
+    canClickMinus: MutableState<Boolean>,
+    canClickPlus: MutableState<Boolean>,
+    maxPlus: Int
+    ) {
+    when(operations) {
+        Operations.Minus -> {
+            canClickMinus.value = count - 1 > 0
+            if (count > 0) {
+                onCountChange.invoke(count- 1)
+            }
+            canClickPlus.value = count - 1 < maxPlus
+        }
+        Operations.Plus -> {
+            canClickPlus.value = count + 1 < maxPlus
+            if (count < maxPlus) {
+                onCountChange.invoke(count + 1)
+            }
+            canClickMinus.value = count + 1 > 0
+        }
+    }
+}
+
+const val MAX_MINUTES = 99
+const val MAX_SECONDS = 60
+
+fun makeTimeText(value: Int) : String =
+    if (value < 10) "0$value" else value.toString()
+
+sealed class Operations {
+    object Plus: Operations()
+    object Minus: Operations()
+}
+
+sealed class TimeUnit {
+    object Seconds: TimeUnit()
+    object Minutes: TimeUnit()
+}
